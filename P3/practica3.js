752447a27e8f47824dc4c74c46c07f6d270e6be0
Andrea <andreamartin@ucm.es>
2018-04-19 15:56:57 +0200

    goombas y bloopas añadidos

--- P3/practica3.js
+++ P3/practica3.js
@@ -38,18 +38,109 @@ var game = function() {
 			this.add('2d, platformerControls');
 			// Write event handlers to respond hook into behaviors.
 			// hit.sprite is called everytime the player collides with a sprite
-			this.on("hit.sprite",function(collision) {
-			// Check the collision, if it's the Tower, you win!
+			/*this.on("hit.sprite",function(collision) {
+				// Check the collision, if it's the Tower, you win!
 				
+			});*/
+
+			this.on("bump.bottom",function(collision) {
+				if(collision.obj.isA("Goomba")) {
+					this.p.vy = -300;					
+				}
 			});
+
+		},
+
+		step: function(p) {
+			if(this.p.y > 600){
+				Q.stageScene('level1');
+			}
 		}
 	});
 
 
+	Q.Sprite.extend("Goomba", {
+		init: function(p) {
+			this._super(p, {
+				sheet: "goomba",
+				sprite: "goomba",
+				frame: 0,
+				vx: 75
+			});
+
+			this.add('2d, aiBounce');
+
+			this.on("bump.left,bump.right,bump.bottom",function(collision) {
+				if(collision.obj.isA("Mario")) {
+					Q.stageScene("level1");
+					collision.obj.destroy();
+				}
+			});
+			// If the enemy gets hit on the top, destroy it
+			// and give the user a "hop"
+			this.on("bump.top",function(collision) {
+				if(collision.obj.isA("Mario")) {
+				
+					this.destroy();
+					collision.obj.p.vy = -300;
+				}
+			});
+
+		},
+
+		/*step: function(p) {
+			if(this.p.vy == 0){
+				this.p.vy = -500;
+			}
+		}*/ 	
+	});
+
+	Q.Sprite.extend("Bloopa", {
+		init: function(p) {
+			this._super(p, {
+				sheet: "bloopa",
+				sprite: "bloopa",
+				frame: 0,
+				vx: 0
+			});
+
+			this.add('2d');
+
+			this.on("bump.left,bump.right,bump.bottom",function(collision) {
+				if(collision.obj.isA("Mario")) {
+					Q.stageScene("level1");
+					collision.obj.destroy();
+				}
+			});
+			// If the enemy gets hit on the top, destroy it
+			// and give the user a "hop"
+			this.on("bump.top",function(collision) {
+				if(collision.obj.isA("Mario")) {
+				
+					this.destroy();
+					collision.obj.p.vy = -300;
+				}
+			});
+
+		},
+
+		step: function(p) {
+			if(this.p.vy == 0){
+				this.p.vy = -Math.floor(Math.random() * (800 - 300) + 300);
+			}
+		}	
+	});
+
 	Q.scene("level1",function(stage) {
 		Q.stageTMX("level.tmx",stage);
 
-		var mario = stage.insert(new Q.Mario({ x: 150, y: 380 })); //añadimos a mario
+		var mario = stage.insert(new Q.Mario({ x: 150, y: 530 })); //añadimos a mario
+
+		var goomba = stage.insert(new Q.Goomba({ x: 600, y: 530 }));
+
+		var bloopa = stage.insert(new Q.Bloopa({ x: 500, y: 530 }));
+		var bloopa = stage.insert(new Q.Bloopa({ x: 400, y: 530 }));
+		var bloopa = stage.insert(new Q.Bloopa({ x: 300, y: 530 }));
 
 		stage.add("viewport").follow(mario,{ x: true, y: false }); //seguimos a mario
 	 	stage.centerOn(150,380);	
@@ -61,8 +152,10 @@ var game = function() {
 	});
 
 
-	Q.load(["mario_small.png", "mario_small.json"], function() {
+	Q.load(["mario_small.png", "mario_small.json", "goomba.png", "goomba.json", "bloopa.png", "bloopa.json"], function() {
 		Q.compileSheets("mario_small.png","mario_small.json");   //cargamos los sprites de mario
+		Q.compileSheets("goomba.png","goomba.json"); // cargamos los sprite del goomba
+		Q.compileSheets("bloopa.png","bloopa.json"); // cargamos los sprite del goomba
     });
 
 
